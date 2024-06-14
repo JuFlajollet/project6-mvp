3dc41843d08b67f798e53309f546f88e4cb9a4f5
JuFlajollet <hika@me.com>
2024-06-14 16:00:15 +0200

    Fixing back app not launching because of db and mapper

--- back/src/main/java/com/openclassrooms/mddapi/mappers/TopicMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mappers/TopicMapper.java
@@ -3,16 +3,9 @@ package com.openclassrooms.mddapi.mappers;
 import com.openclassrooms.mddapi.dto.TopicDTO;
 import com.openclassrooms.mddapi.models.Topic;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-@Mapper
-public interface TopicMapper {
-    Topic toEntity(TopicDTO dto);
-
-    TopicDTO toDto(Topic entity);
-
-    List<Topic> toEntity(List<TopicDTO> dtoList);
-
-    List<TopicDTO> toDto(List<Topic> entityList);
+@Component
+@Mapper(componentModel = "spring")
+public interface TopicMapper extends EntityMapper<TopicDTO, Topic> {
 }
