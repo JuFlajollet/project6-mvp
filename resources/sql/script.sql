a95defa39b5dc68b3dd1b98f74a878f141d5dd16
JuFlajollet <hika@me.com>
2024-06-19 15:52:56 +0200

    Updating script.sql to create users table and example user and adding postman collection with topic and user requests

--- resources/sql/script.sql
+++ resources/sql/script.sql
@@ -1,7 +1,16 @@
 CREATE TABLE `TOPICS` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(50),
-  `description` VARCHAR(2000),
+  `name` VARCHAR(100),
+  `description` VARCHAR(3000),
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `USERS` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(100),
+  `email` VARCHAR(255),
+  `password` VARCHAR(255),
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -9,3 +18,6 @@ CREATE TABLE `TOPICS` (
 INSERT INTO TOPICS (name, description)
 VALUES ('Java', 'Object-Oriented Programming Language'),
        ('Angular', 'Typescript Web Framework');
+
+INSERT INTO USERS (username, email, password)
+VALUES ('Test', 'test@gmail.com', 'Test12345!');
